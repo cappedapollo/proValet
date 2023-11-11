3abd947a5664d8c974b62848becb3b5da01e0f23
cappedapollo <cappedapollo@gmail.com>
2023-11-10 23:50:21 -0500

    update php socket

---
+++ Deliverables/server.php
@@ -0,0 +1,39 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require 'vendor/autoload.php';
+
+class MyWebSocketServer implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        echo "Received message: $msg\n";
+        $from->send("Server received your message: $msg");
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "Error: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new MyWebSocketServer()
+        )
+    ),
+    12345
+);
+
+$server->run();
