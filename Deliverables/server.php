a169c2bcb61c91113253346021bbac7fc07a34a9
cappedapollo <cappedapollo@gmail.com>
2023-11-11 20:07:18 -0500

    update server.php

--- Deliverables/server.php
+++ Deliverables/server.php
@@ -7,13 +7,45 @@ use Ratchet\WebSocket\WsServer;
 
 require 'vendor/autoload.php';
 
+// SQLite database file path
+$dbFile = './db.sqlite';
+
+try {
+    // Create a PDO connection to the SQLite database
+    $pdo = new PDO("sqlite:$dbFile");
+
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected to the SQLite database successfully\n";
+
+    try {
+        // Create a table if it doesn't exist
+        $pdo->exec("CREATE TABLE IF NOT EXISTS garages (
+                        id INTEGER PRIMARY KEY,
+                        x INTEGER NOT NULL,
+                        y INTEGER NOT NULL,
+                        w INTEGER NOT NULL,
+                        h INTEGER NOT NULL,
+                        f INTEGER DEFAULT 0
+                    )");
+        echo "Table created successfully\n";
+    } catch (PDOException $e) {
+        die("Table creation failed: " . $e->getMessage());
+    }
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
 class MyWebSocketServer implements MessageComponentInterface {
     public function onOpen(ConnectionInterface $conn) {
         echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        echo "Received message: $msg\n";
+        // echo "Received message: $msg\n";
+        $obj = json_decode($msg);
         $from->send("Server received your message: $msg");
     }
 
